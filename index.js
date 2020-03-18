a89030f44e53b429908a0462844bb1e628393e72
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-03-18 23:46:07 +0300

    add nav base

--- index.js
+++ index.js
@@ -4,26 +4,19 @@ const cors = require('cors');
 
 app.use(cors());
 
+const base = {
+  navList: [
+    'Торты',
+    'Десерты',
+    'Веган',
+    'Контакты'
+  ]
+}
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-const dataJSON = [
-  'List 1',
-  'List 2',
-  'List 3',
-  'List 4',
-  'List 5',
-  'List 6'
-]
-
-let count = 1;
-
-app.get('/json', function (req, res) {
-  console.log(++count)
-  res.json(dataJSON);
+  res.json(base);
 });
 
 app.listen(5555, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 5555!');
 });

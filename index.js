ac418ca52fb3e6611a92d31c48940bf28cd50436
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-12 21:11:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-// const cors = require('cors');
+const gamesJSON = fs.readFileSync('./json/games.json');
+const cors = require('cors');
 // app.use(cors());
-const PORT = process.env.PORT || 80;
 
-const base = {
-  navList: [
-    'Торты',
-    'Десерты',
-    'Веган',
-    'Контакты'
-  ]
+const PORT = process.env.PORT || 80;
+const whitelist = [
+  'https://alimzhanovamir.github.io/egamings-test-react/',
+  'http://localhost:3000/egamings-test-react'
+];
+
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
+
 
 app.get('/', function (req, res) {
   res.end(`
@@ -35,6 +44,10 @@ app.get('/about', function (req, res) {
       </ul>
     </nav>  
   `);
+});
+
+app.get('/games', cors(corsOptionsDelegate),function (req, res) {
+  res.json(gamesJSON);
 });
 
 app.listen(PORT, function () {

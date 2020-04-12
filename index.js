aea6de9061a9ef35d21255566018c6a1a104f32a
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-12 20:31:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
+const PORT = process.env.PORT || 80;
 
 const base = {
   navList: [
@@ -14,9 +14,29 @@ const base = {
 }
 
 app.get('/', function (req, res) {
-  res.json(base);
+  res.end(`
+    <h1>Test Server</h1>
+    <nav>
+      <ul>
+        <li><a href="/">Go home page</a></li>
+        <li><a href="/about">Go about page</a></li>
+      </ul>
+    </nav>
+  `);
 });
 
-app.listen(5555, function () {
-  console.log('Example app listening on port 5555!');
+app.get('/about', function (req, res) {
+  res.end(`
+    <h1>About</h1>
+    <nav>
+      <ul>
+        <li><a href="/">Go home page</a></li>
+        <li><a href="/about">Go about page</a></li>
+      </ul>
+    </nav>  
+  `);
+});
+
+app.listen(PORT, function () {
+  console.log('Server has been started...');
 });
